c37ac25788ac2c687df1f2887f2ee4276ecbbbf3
dyson1602 <cclark000@citymail.cuny.edu>
2020-11-30 15:29:59 -0500

    seed data works. don't forget to db:seed (13 mins)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+
+file = File.open('/Users/chrisclark/Flatiron/code/Mod3/project/weather-app-backend/db/city.list.json')
+
+cities = JSON.parse(file.read)
+
+cities.each do |city|
+  City.create(name: city["name"], state: city["state"], country: city["country"], 
+  lat: city["coord"]["lat"], lon: city["coord"]["lon"], search_id: city["id"])
+end
+
+# {"id"=>8260265, "name"=>"Buutuo", "state"=>"", "country"=>"LR", "coord"=>{"lon"=>-8.3428, "lat"=>6.8275}},
